0bb760d58935be588700a50077ecd49a3166183a
Michael Peyper <michael.peyper@ioof.com.au>
2019-09-27 14:20:27 +1000

    Fix examples after updates

--- examples/microfrontends/webpack.config.js
+++ examples/microfrontends/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: [/node_modules/],
         query: {
-          presets: [['@babel/env', { modules: false, useBuiltIns: 'usage' }], '@babel/react']
+          presets: [['@babel/env', { modules: false, useBuiltIns: 'usage', corejs: 3 }], '@babel/react']
         }
       }
     ]
